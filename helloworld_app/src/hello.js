737c0dc59029bd1290212f51d1ce3b8f8c90f4d4
Priyaksm12 <priyankaks.priya@gmail.com>
2022-04-05 22:01:15 +0530

    UC4 code change

--- helloworld_app/src/hello.js
+++ helloworld_app/src/hello.js
@@ -2,17 +2,21 @@ import React from 'react';
 import logo from './logo.png'
 
 class Hello extends React.Component{
+    url="https:www.bridgelabz.com/"
     constructor(){
         super()
         this.state = {
             title : 'hello from bridgelabz'
         }
     }
+    imageclick = () =>{
+            window.open(this.url,'_blank');
+    }
     render(){
         return(
             <div>
                 <h1 style={{"color":"red"}}>{this.state.title}</h1>
-                <img src={logo} className="logo"/>
+                <img src={logo} className="logo" onClick={this.imageclick}/>
             </div>
         );
     }
